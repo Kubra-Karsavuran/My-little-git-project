8eedad20a91a534661da6e3baaa55e1e0c7e99a4
bi dolu yazılım <kubra@gmail.com>
2022-08-16 11:27:30 +0300

    yorum yansıtılacak sayfa guncellendı

---
+++ commentYansit.php
@@ -0,0 +1,13 @@
+<?php 
+if ( isset( $_POST['nameveri'] ) ) {
+	if (isset( $_POST['emailveri'] )) {
+		if (isset( $_POST['mesajveri'] )) {
+			echo "name:".$nameveri."email:".$emailveri."mesaj:".$mesajveri;
+		}
+	}
+}
+
+ ?>
+
+ 
+
